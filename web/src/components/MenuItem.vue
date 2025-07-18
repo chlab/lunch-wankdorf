bdae01f0244f1079a6ec2e4800733bcde688e6eb
e553956 <christof.leuenberger@sbb.ch>
2025-07-18 12:55:01 +0200

    feat: switch to vegi icon instead of text

--- web/src/components/MenuItem.vue
+++ web/src/components/MenuItem.vue
@@ -66,7 +66,11 @@ const props = defineProps({
           <span class="text-xs ml-2">{{ foodtruck }}</span>
         </div>
 
-        <span v-if="type === 'vegetarian'" class="px-3 py-1 text-xs bg-green-100 text-green-800 rounded-full">Vegi</span>
+        <span v-if="type === 'vegetarian'" class="px-3 py-1 text-xs bg-green-100 text-green-800 rounded-full">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"  class="size-4" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z"></path><path d="M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12"></path>
+          </svg>
+        </span>
 
         <a v-if="link" :href="link" class="bg-gray-200 rounded-full py-1 px-1 ml-auto hover:bg-gray-300" target="_blank">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
